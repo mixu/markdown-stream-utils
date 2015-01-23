7de83dab0edabf29c7e7bf822271b51111df03ad
Mikito Takada <mikito.takada@gmail.com>
2015-01-22 21:00:21 -0800

    Add missing features, add tests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.annotateMdHeadings = require('./lib/annotate-md-headings');
-exports.highlightJs = require('./lib/highlight-js');
+exports.highlight = require('./lib/highlight');
 exports.parseHeader = require('./lib/parse-header');
 exports.parseMd = require('./lib/parse-md');
 exports.convertMd = require('./lib/convert-md');
